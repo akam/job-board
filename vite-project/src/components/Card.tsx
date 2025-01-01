b37dd70a59a5f43648b6d7886e8e311c545df453
akam <albertkam91@gmail.com>
2025-01-01 19:13:41 +1100

    refactored code

--- vite-project/src/components/Card.tsx
+++ vite-project/src/components/Card.tsx
@@ -1,14 +1,27 @@
-import { PropsWithChildren } from "react";
-
 interface cardProps {
+    title: string
+    description: string
+    buttonText: string
     bg?: string
 }
 
-function Card (props: PropsWithChildren<cardProps>){
+function Card (props: cardProps){
+    
     const backgroundColor = props.bg ? props.bg : 'bg-gray-100';
+
     return (
         <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
-            {props.children}
+            <h2 className='text-2xl font-bold'>
+                {`For ${props.title}`}
+            </h2>
+            <p className='mt-2 mb-4'>
+                {props.description}
+            </p>
+            <button
+              className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700'
+            >
+                {props.buttonText}
+            </button>
         </div>)
   };
   
