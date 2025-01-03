4eab67271ec68c0d426a052b739ad24fe8318411
akam <albertkam91@gmail.com>
2025-01-03 18:11:26 +1100

    added job page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -7,14 +7,17 @@ import {
 import HomePage from './pages/HomePage.tsx';
 import MainLayout from './layouts/MainLayout.tsx';
 import JobsPage from './pages/JobsPage.tsx';
-import NotFoundPage from './pages/NotFoundPage.tsx'
+import NotFoundPage from './pages/NotFoundPage.tsx';
+import JobPage, { jobLoader } from './pages/JobPage.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
         <Route index element={<HomePage/>}/>
         <Route path='/jobs' element={<JobsPage/>}/>
+        <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader}/>
         <Route path='*' element={<NotFoundPage/>}/>
+
     </Route>
   )
 );
